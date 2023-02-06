33aaea958780522d53b47d5c294d789e3ba9949d
thanhnt <nguyentruongthanh0612@gmail.com>
2023-02-06 17:11:44 +0700

    fix path

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import mongoose from 'mongoose';
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000;
-const URI = process.env.MONGO_DB;
+const URI = process.env.MONGODB_URL;
 
 
 app.use(express.json())
